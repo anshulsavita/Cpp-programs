1fab76b92bb962d986652319e5474083e88a7bdb
Anshul savita <anshulsavita370@gmail.com>
2026-09-24 22:20:11 +0530

    added lecture 10 of cpp where first program of OOPS has been covered

---
+++ lecture10.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+using namespace std;
+// components of OOPS
+// ---------------------
+// class & objects
+// ----------------------
+// Syntax -->
+// class <classname>{
+//     private:
+//         =======
+//         =======
+//     public:
+//         =======
+//         =======
+// };
+
+// Private members can be accessed only inside the class.
+// Public members can be accessed from outside the class.
+
+// for example -->
+class Student{
+    private:
+        /* DATA MEMBERS / INSTANCE VARIABLES
+        Variables declared inside a class are called data members.*/
+        int rollno;
+        string name;
+        int p,c,m;
+    public:
+        /* MEMBER FUNCTION / METHOD
+        A function declared inside a class is called a member function.*/
+        void getStudent(){
+            cout<<"Enter Rollno:";
+            cin>>rollno;
+            cout<<"Enter Name:";
+            cin>>name;
+            cout<<"Enter P C M Marks:";
+            cin>>p>>c>>m;
+        }
+
+        void showResult(){
+            float t=p+c+m;
+            float pr=t/3;
+            cout<<"Rollno:"<<rollno<<endl;
+            cout<<"Name"<<name<<endl;
+            cout<<"Physics:"<<p<<endl;
+            cout<<"Chemistry:"<<c<<endl;
+            cout<<"Maths:"<<m<<endl;
+            cout<<"Total:"<<t<<endl;
+            cout<<"Percentage:"<<pr<<endl;
+            
+        }
+};
+int main(){
+    Student S1,S2;
+    //cout<<sizeof(S1);
+    S1.getStudent();
+    S2.getStudent();
+
+    S1.showResult();
+    S2.showResult();
+} 
